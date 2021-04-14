af8be1c370275626c3a5a3030ec1ee10c45dd3f7
VelixHalim <55150412+VelixHalim@users.noreply.github.com>
2021-04-15 00:04:07 +0700

    insert job post

--- app/src/main/java/id/ac/umn/mobile_jobfinder/InsertJobPostActivity.java
+++ app/src/main/java/id/ac/umn/mobile_jobfinder/InsertJobPostActivity.java
@@ -4,36 +4,39 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-//import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
-public class All_JobActivity extends AppCompatActivity {
+public class InsertJobPostActivity extends AppCompatActivity {
     //variable toolbar
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
     //end variabel toolbar
 
-    private FloatingActionButton fabBtn;
-
+    //variabel post job
+    EditText edtJobTitle, edtJobDesc, edtSkill, edtSalary;
+    Button btnPost;
+    //end variable post job
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_job);
+        setContentView(R.layout.activity_insert_job_post);
+
+        //toolbar
         //untuk menu start
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.insert_job_Drawlayout);
+        navigationView = findViewById(R.id.insert_nav_view);
+        toolbar = (Toolbar) findViewById(R.id.insert_job_toolbar);
 
         setSupportActionBar(toolbar);
 
@@ -64,15 +67,22 @@ public class All_JobActivity extends AppCompatActivity {
         });
         //navigationView.setNavigationItemSelectedListener(this);
         // end
+        //end toolbar
 
-        //fab start
-        fabBtn = findViewById(R.id.fab_add);
-        fabBtn.setOnClickListener(new View.OnClickListener() {
+        //post job
+        btnPost = findViewById(R.id.btnPost_Job);
+        edtJobTitle = findViewById(R.id.edtJobTitle);
+        edtJobDesc = findViewById(R.id.edtJobDesc);
+        edtSkill = findViewById(R.id.edtSkill);
+        edtSalary = findViewById(R.id.edtSalary);
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(All_JobActivity.this, InsertJobPostActivity.class));
+
             }
         });
+        //end post job
     }
 
     @Override
@@ -83,10 +93,4 @@ public class All_JobActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//        return true;
-//    }
-
 }
--- app/src/main/java/id/ac/umn/mobile_jobfinder/MainActivity.java
+++ app/src/main/java/id/ac/umn/mobile_jobfinder/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         btnPostJob.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, All_JobActivity.class));
+                startActivity(new Intent(MainActivity.this, InsertJobPostActivity.class));
             }
         });
     }
