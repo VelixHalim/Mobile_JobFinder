3f3147e721a8b5297801ed9d8a16e4e1a70f23e5
VelixHalim <55150412+VelixHalim@users.noreply.github.com>
2021-04-14 14:43:58 +0700

    see all job

---
+++ app/src/main/java/id/ac/umn/mobile_jobfinder/All_Job.java
@@ -0,0 +1,31 @@
+package id.ac.umn.mobile_jobfinder;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class All_Job extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all__job);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+//        toolbar = findViewById(R.id.to)
+//        setSupportActionBar(toolbar);
+        // navigation drawer menu
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+}
